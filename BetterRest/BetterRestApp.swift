c816268782b438aef0e16a0aacbc480ec1006610
Anton Serdyuchenko <anton415@gmail.com>
2024-12-16 14:08:09 +0300

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Anton Serdyuchenko on 16.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
